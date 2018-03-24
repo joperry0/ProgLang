549c555319f5bc53eea1503439a84426dadcd391
catte <catte@leftist.io>
2018-03-23 22:05:28 -0500

    added python interpreter of a microscala framework

---
+++ HW7/Test1.scala
@@ -0,0 +1,19 @@
+
+object Test1
+{
+  def main (args : Array [String]) {
+    var q : Int = 0;
+    var r : Int = 0;
+    var x : Int = 0;
+    var y : Int = 0;
+    x = 32; y = 5;
+    r = x;
+    while (r >= y)
+    {  
+      q = q + 1;
+      r = r - y;
+    }  
+    println (q);
+    println (r);
+  }
+}
